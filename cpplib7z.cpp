8fa831ef0bf23ca4ba15ad61ca6034013e95ef91
Mark Harviston <harvimt@pdx.edu>
2012-12-14 13:46:29 -0800

    improvements to cpplib7zip

--- cpplib7z.cpp
+++ cpplib7z.cpp
@@ -8,17 +8,12 @@
  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  */
 
 #include <cstdio>
-
-//#include <iostream>
-
-#include <fstream>
-#include <ios>
 #include <utf8.h>//C++ UTF-8 Library
 #include "cpplib7z.h"
-
 
 // In Stream (FD)
 
@@ -38,16 +33,31 @@ C7ZipInStreamFWrapper::C7ZipInStreamFWrapper (const string filename)
 		utf8to32(filename.begin() + ext_pos, filename.end(), back_inserter(m_ext));
 	}
 
-	//std::wcout << "m_ext: " << m_ext << std::endl;
-
-	//Get File Size
-	fseek(m_fd, 0L, SEEK_END);
-	m_size = ftell(m_fd);
-	fseek(m_fd, 0L, SEEK_SET);
+	calc_size();
 }
 
 
 C7ZipInStreamFWrapper::C7ZipInStreamFWrapper (FILE* fd, const wstring ext) : m_fd(fd), m_ext(ext) {
+	calc_size();
+}
+
+C7ZipInStreamFWrapper::C7ZipInStreamFWrapper (FILE* fd, const string ext) : m_fd(fd) {
+	using namespace utf8;
+
+	if(sizeof(wchar_t) == 2) {
+		utf8to16(ext.begin(), ext.end(), back_inserter(m_ext));
+	}else if(sizeof(wchar_t) == 4){
+		utf8to32(ext.begin(), ext.end(), back_inserter(m_ext));
+	}
+
+	calc_size();
+}
+
+C7ZipInStreamFWrapper::~C7ZipInStreamFWrapper() {
+	fclose(m_fd);
+}
+
+void C7ZipInStreamFWrapper::calc_size(){
 	if(!m_fd) return;
 
 	//Get File Size
@@ -56,14 +66,7 @@ C7ZipInStreamFWrapper::C7ZipInStreamFWrapper (FILE* fd, const wstring ext) : m_f
 	fseek(m_fd, 0L, SEEK_SET);
 }
 
-C7ZipInStreamFWrapper::~C7ZipInStreamFWrapper()
-{
-	fclose(m_fd);
-}
-
-
-int C7ZipInStreamFWrapper::Read(void *data, unsigned int size, unsigned int *processedSize)
-{
+int C7ZipInStreamFWrapper::Read(void *data, unsigned int size, unsigned int *processedSize) {
 	if(!m_fd) return 1;
 
 	int count = fread(data, 1, size, m_fd);
@@ -78,8 +81,7 @@ int C7ZipInStreamFWrapper::Read(void *data, unsigned int size, unsigned int *pro
 }
 
 
-int C7ZipInStreamFWrapper::Seek(__int64 offset, unsigned int seekOrigin, unsigned __int64 *newPosition)
-{
+int C7ZipInStreamFWrapper::Seek(__int64 offset, unsigned int seekOrigin, unsigned __int64 *newPosition) {
 	if(!m_fd) return 1;
 	if (seekOrigin > 2) {
 		return 1;
@@ -102,16 +104,14 @@ int C7ZipInStreamFWrapper::Seek(__int64 offset, unsigned int seekOrigin, unsigne
 }
 
 
-int C7ZipInStreamFWrapper::GetSize(unsigned __int64 * size)
-{
+int C7ZipInStreamFWrapper::GetSize(unsigned __int64 * size) {
 	if(size) *size = m_size;
 	return 0;
 }
 
 
 // Out Stream (FD)
-C7ZipOutStreamFWrapper::C7ZipOutStreamFWrapper (const string filename)
-{
+C7ZipOutStreamFWrapper::C7ZipOutStreamFWrapper (const string filename) {
 	m_fd = fopen(filename.c_str(), "wb");
 
 	//Get File Size
@@ -123,14 +123,12 @@ C7ZipOutStreamFWrapper::C7ZipOutStreamFWrapper (const string filename)
 
 C7ZipOutStreamFWrapper::C7ZipOutStreamFWrapper (FILE* fd): m_fd (fd) { }
 
-C7ZipOutStreamFWrapper::~C7ZipOutStreamFWrapper()
-{
+C7ZipOutStreamFWrapper::~C7ZipOutStreamFWrapper() {
 	fclose(m_fd);
 }
 
 
-int C7ZipOutStreamFWrapper::Write(const void *data, unsigned int size, unsigned int *processedSize)
-{
+int C7ZipOutStreamFWrapper::Write(const void *data, unsigned int size, unsigned int *processedSize) {
 	int count = fwrite(data, 1, size, m_fd);
 	if(ferror(m_fd)) {
 		return 1;
@@ -146,8 +144,7 @@ int C7ZipOutStreamFWrapper::Write(const void *data, unsigned int size, unsigned 
 }
 
 
-int C7ZipOutStreamFWrapper::Seek(__int64 offset, unsigned int seekOrigin, unsigned __int64 *newPosition)
-{
+int C7ZipOutStreamFWrapper::Seek(__int64 offset, unsigned int seekOrigin, unsigned __int64 *newPosition) {
 	if(!m_fd) return 1;
 
 	if (seekOrigin > 2) {
@@ -168,161 +165,4 @@ int C7ZipOutStreamFWrapper::Seek(__int64 offset, unsigned int seekOrigin, unsign
 	}
 
 	return 0;
-}
-
-
-// In-Stream io-streams
-C7ZipInStreamSWrapper::C7ZipInStreamSWrapper (const string filename)
-{
-	using namespace std;
-	using namespace utf8;
-
-	m_stream = new ifstream(filename.c_str(), ifstream::binary | ifstream::in);
-
-	size_t ext_pos = filename.rfind('.') + 1;
-	if(sizeof(wchar_t) == 2) {
-		utf8to16(filename.begin() + ext_pos, filename.end(), back_inserter(m_ext));
-	}else if(sizeof(wchar_t) == 4){
-		utf8to32(filename.begin() + ext_pos, filename.end(), back_inserter(m_ext));
-	}
-
-	m_stream->seekg (0, ios::end);
-	m_size = m_stream->tellg();
-	m_stream->seekg (0, ios::beg);
-}
-
-
-C7ZipInStreamSWrapper::C7ZipInStreamSWrapper (std::istream * stream, const wstring ext) : m_stream(stream), m_ext(ext)
-{
-	using namespace std;
-
-	m_stream->seekg (0, ios::end);
-	m_size = m_stream->tellg();
-	m_stream->seekg (0, ios::beg);
-}
-
-
-C7ZipInStreamSWrapper::~C7ZipInStreamSWrapper()
-{
-	delete m_stream;
-	m_stream = NULL;
-}
-
-
-int C7ZipInStreamSWrapper::Read(void *data, unsigned int size, unsigned int *processedSize)
-{
-	m_stream->read(static_cast<char*>(data), size);
-
-	if(m_stream->fail()) {
-		return 1;
-	}
-
-	if(processedSize) {
-		*processedSize = m_stream->gcount();
-	}
-
-	return 0;
-}
-
-
-int C7ZipInStreamSWrapper::Seek(__int64 offset, unsigned int seekOrigin, unsigned __int64 *newPosition)
-{
-	using namespace std;
-
-	static const ios_base::seekdir dir[] = {
-		ios::beg,
-		ios::cur,
-		ios::end
-	};
-
-	if (seekOrigin > 0) {
-		return 1;
-	}
-
-	m_stream->seekg(offset, dir[seekOrigin]);
-
-	if(m_stream->fail()) {
-		return 1;
-	}
-
-	if (newPosition != NULL) {
-		*newPosition = m_stream->tellg();
-		if(m_stream->fail()) {
-			return 1;
-		}
-	}
-
-	return 0;
-}
-
-
-int C7ZipInStreamSWrapper::GetSize(unsigned __int64 * size)
-{
-	if(size) *size = m_size;
-	return 0;
-}
-
-
-// Out Stream IO-Streams
-C7ZipOutStreamSWrapper::C7ZipOutStreamSWrapper (const string filename) : m_size(0)
-{
-	using namespace std;
-	m_stream = new fstream(filename.c_str(), ios::in | ios::out | ios::binary);
-}
-
-
-C7ZipOutStreamSWrapper::C7ZipOutStreamSWrapper (std::ostream * stream): m_stream(stream), m_size(0) {}
-
-C7ZipOutStreamSWrapper::~C7ZipOutStreamSWrapper()
-{
-	delete m_stream;
-}
-
-
-int C7ZipOutStreamSWrapper::Write(const void *data, unsigned int size, unsigned int *processedSize)
-{
-	m_stream->write(static_cast<const char*>(data), size);
-	m_stream->flush();
-
-	if(m_stream->fail()) {
-		return 1;
-	}
-
-	if(processedSize) {
-		*processedSize = size;
-	}
-
-	return 0;
-}
-
-
-int C7ZipOutStreamSWrapper::Seek(__int64 offset, unsigned int seekOrigin, unsigned __int64 *newPosition)
-{
-	using namespace std;
-
-	static const ios_base::seekdir dir[] = {
-		ios::beg,
-		ios::cur,
-		ios::end
-	};
-
-	if (seekOrigin > 0) {
-		return 1;
-	}
-
-	m_stream->seekp(offset, dir[seekOrigin]);
-
-	if(m_stream->fail()) {
-		return 1;
-	}
-
-	if (newPosition != NULL) {
-		*newPosition = m_stream->tellp();
-		if(m_stream->fail()) {
-			return 1;
-		}
-	}
-
-	return 0;
-
 }
--- cpplib7z.h
+++ cpplib7z.h
@@ -8,30 +8,39 @@
  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * C++ lib7Zip helpers
+ * ===================
+ * This file provides implementations on top of C7ZipInStream and C7ZipOutStream on top of POSIX file-descriptors (FILE*)
+ *
  */
 
 #ifndef CPPLIB7ZIP
 #define CPPLIB7ZIP
 
 #include <lib7zip.h>
-#include <istream>
-#include <ostream>
 
 /**
  * Implementation of 7-Zip In Stream based on cstdio File-Pointers (FILE*)
  */
 class C7ZipInStreamFWrapper : public C7ZipInStream
 {
+	/*
+	 * the file-pointer passed in will be closed automatically when the InStream is destroyed
+	 */
 	private:
 		FILE* m_fd;
 		wstring m_ext;
 		unsigned __int64 m_size;
-		bool owns_pointer;
+		void calc_size();
 
 	public:
+		///This constructor has no way of telling you the file failed to open, it's recommended you use the other constructor
 		C7ZipInStreamFWrapper (const string filename);
 
+		///Be sure the file is opened in a binary read mode e.g. fopen(filename, "rb")
 		C7ZipInStreamFWrapper (FILE* fd, const wstring ext);
+		C7ZipInStreamFWrapper (FILE* fd, const string ext);
 
 		virtual ~C7ZipInStreamFWrapper();
 
@@ -49,13 +58,19 @@ class C7ZipInStreamFWrapper : public C7ZipInStream
  */
 class C7ZipOutStreamFWrapper : public C7ZipOutStream
 {
+	/*
+	 * the file-pointer passed in will be closed automatically when the OutStream is destroyed
+	 */
 	private:
 		FILE * m_fd;
 		int m_size;
 
 	public:
+		///This constructor has no way of telling you the file failed to open, it's recommended you use the other constructor
+		///However, Opening a file for writing has (slightly) fewer things that can go wrong (if the file doesn't exist it is created)
 		C7ZipOutStreamFWrapper (const string filename);
 
+		///Be sure the file is opened in a binary write mode e.g. fopen(filename, "wb")
 		C7ZipOutStreamFWrapper (FILE* fd);
 
 		~C7ZipOutStreamFWrapper();
@@ -69,67 +84,7 @@ class C7ZipOutStreamFWrapper : public C7ZipOutStream
 		//placebo method, snark, snark
 		int SetSize(unsigned __int64 size) const
 		{
-			(void)size; return 0;
+			(void)size; //casting a var to void, silences the unnused param error
+			return 0;
 		};
-		//casting a var to void, silences the unnused param error
-
 };
-
-/**
- * Implementation of 7-Zip In Stream based on C++ IO std::istream
- */
-class C7ZipInStreamSWrapper : public C7ZipInStream
-{
-	private:
-		std::istream * m_stream;
-		wstring m_ext;
-		unsigned __int64 m_size;
-
-	public:
-		C7ZipInStreamSWrapper (const string filename);
-
-		C7ZipInStreamSWrapper (std::istream * stream, const wstring ext);
-
-		~C7ZipInStreamSWrapper();
-
-		wstring GetExt() const { return m_ext; }
-
-		int Read(void *data, unsigned int size, unsigned int *processedSize);
-
-		int Seek(__int64 offset, unsigned int seekOrigin, unsigned __int64 *newPosition);
-
-		int GetSize(unsigned __int64 * size);
-};
-
-/**
- * Implementation of 7-Zip Out Stream based on C++ IO std::ostream
- */
-class C7ZipOutStreamSWrapper : public C7ZipOutStream
-{
-	private:
-		std::ostream * m_stream;
-		int m_size;
-
-	public:
-		C7ZipOutStreamSWrapper (const string filename);
-
-		C7ZipOutStreamSWrapper (std::ostream * stream);
-
-		~C7ZipOutStreamSWrapper();
-
-		int Write(const void *data, unsigned int size, unsigned int *processedSize);
-
-		int Seek(__int64 offset, unsigned int seekOrigin, unsigned __int64 *newPosition);
-
-		int GetFileSize() const { return m_size; }
-
-		//placebo method, snark, snark
-		int SetSize(unsigned __int64 size) const
-		{
-			(void)size; return 0;
-		};
-		//casting a var to void, silences the unnused param error
-
-};
-
-#endif
