a509b7d95f34a624d0da03d4fc0fa142f66fca54
Mark Harviston <mark.harviston@gmail.com>
2015-12-30 19:36:25 -0800

    initial broken code

---
+++ clib7zip.cpp
@@ -0,0 +1,222 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <wchar.h>
+#include <stdint.h>
+//#include <dlfcn.h>
+#include <errno.h>
+#include <stdarg.h>
+
+#include "StdAfx.h"
+#include "Windows/PropVariant.h"
+
+#include "Common/MyCom.h"
+
+#include "7zip/IStream.h"
+#include "7zip/Archive/IArchive.h"
+#include "7zip/IPassword.h"
+
+#include "clib7zip.h"
+
+static void (*log_debug_cb)(const char*) = NULL;
+
+void set_logger_cb(void(*cb)(const char*)) {
+    log_debug_cb = cb;
+}
+
+static inline void LOG_DEBUG(const char* fmt, ...){
+    char buffer[256];
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(buffer, sizeof(buffer), fmt, args);
+    va_end(args);
+    if (log_debug_cb == NULL){
+        fprintf(stderr, "%s\n", buffer);
+    } else {
+        log_debug_cb(buffer);
+    }
+}
+
+class CFileStream:
+  public IInStream,
+  public IOutStream,
+  public IStreamGetSize,
+  public CMyUnknownImp
+{
+public:
+  CFileStream(FILE* file_ptr_in) : file_ptr(file_ptr_in) {}
+  virtual ~CFileStream() {}
+  MY_UNKNOWN_IMP2(IInStream, IStreamGetSize)
+
+  STDMETHOD(Read)(void *data, UInt32 size, UInt32 *processedSize);
+  STDMETHOD(Write)(void *data, UInt32 size, UInt32 *processedSize);
+  STDMETHOD(Seek)(Int64 offset, UInt32 seekOrigin, UInt64 *newPosition);
+
+  STDMETHOD(GetSize)(UInt64 *size);
+private:
+  FILE* file_ptr;
+};
+
+STDMETHODIMP CFileStream::Read (void *data, UInt32 size, UInt32 *processedSize) {
+    LOG_DEBUG("FileStream::Read");
+    if(processedSize !=  NULL){
+        *processedSize = static_cast<uint32_t>(fread(data, sizeof(uint8_t), size, file_ptr));
+    }else{
+        fread(data, sizeof(uint8_t), size, file_ptr);
+    }
+    return S_OK;
+}
+
+STDMETHODIMP CFileStream::Write (void *data, UInt32 size, UInt32 *processedSize) {
+    LOG_DEBUG("FileStream::Write");
+    if(processedSize !=  NULL){
+        *processedSize = static_cast<uint32_t>(fwrite(data, sizeof(uint8_t), size, file_ptr));
+    }else{
+        fwrite(data, sizeof(uint8_t), size, file_ptr);
+    }
+    return S_OK;
+}
+
+#ifdef WIN32
+#define fseeko64 _fseeki64
+#define ftello64 _ftelli64
+typedef __int64 off64_t;
+#endif
+
+STDMETHODIMP CFileStream::Seek (Int64 offset, UInt32 seekOrigin, UInt64 *newPosition){
+    LOG_DEBUG("CFileStream::Seek(%lld, %u, %p)", offset, seekOrigin, newPosition);
+    if (file_ptr == NULL){ LOG_DEBUG("file_ptr is NULL"); return E_ABORT; };
+    if(fseeko64(file_ptr, offset, seekOrigin) == -1){
+        perror("fseek failed");
+        return E_ABORT;
+    }
+    if(newPosition != NULL){
+        *newPosition = ftello64(file_ptr);
+    }
+    return S_OK;
+}
+
+STDMETHODIMP CFileStream::GetSize(UInt64 *size){
+    LOG_DEBUG("CFileStream::GetSize(%p)", size);
+    if (size == NULL) { return S_OK; }
+    off64_t offset = ftello64(file_ptr);
+    if(fseeko64(file_ptr, 0, SEEK_END) == -1){
+        perror("fseek failed");
+    }
+    *size = ftell(file_ptr);
+    if(fseeko64(file_ptr, offset, SEEK_SET) == -1){
+        perror("fseek failed");
+    }
+    return S_OK;
+}
+
+class CArchiveOpenCallback:
+  public IArchiveOpenCallback,
+  public ICryptoGetTextPassword,
+  public CMyUnknownImp
+{
+public:
+    MY_UNKNOWN_IMP1(ICryptoGetTextPassword)
+
+    CArchiveOpenCallback(void* data)
+        _get_password_callback p_cb,
+        _set_total_callback t_cb, _set_completed_callback c_cb
+    ) :
+        data(data),
+        user_password(password), get_password_callback(p_cb),
+        set_total_callback(t_cb), set_completed_callback(c_cb)
+    {};
+
+    STDMETHOD(SetTotal)(const UInt64 *files, const UInt64 *bytes);
+    STDMETHOD(SetCompleted)(const UInt64 *files, const UInt64 *bytes);
+    STDMETHOD(CryptoGetTextPassword)(BSTR *password);
+private:
+    void* data;
+    wchar_t* user_password;
+};
+
+STDMETHODIMP CArchiveOpenCallback::SetTotal(const UInt64 * files, const UInt64 * bytes)
+{
+    LOG_DEBUG("CArchiveOpenCallback::SetTotal");
+	return S_OK;
+    //if (set_total_callback == NULL) return S_OK;
+    //return set_total_callback(data, (const uint64_t*)(files), (const uint64_t*)(bytes));
+}
+
+STDMETHODIMP CArchiveOpenCallback::SetCompleted(const UInt64 * files, const UInt64 * bytes)
+{
+    LOG_DEBUG("CArchiveOpenCallback::SetCompleted");
+    if (set_completed_callback == NULL) return S_OK;
+    return set_completed_callback(data, (const uint64_t*)(files), (const uint64_t*)(bytes));
+}
+
+STDMETHODIMP CArchiveOpenCallback::CryptoGetTextPassword(BSTR *password)
+{
+    LOG_DEBUG("CArchiveOpenCallback::CryptoGetTextPassword");
+    if (get_password_callback != NULL){
+        return get_password_callback(data, password);
+    } else if (user_password != NULL && password != NULL){
+        *password = user_password;
+        return S_OK;
+    } else {
+        return E_ABORT;
+    }
+}
+
+PROPVARIANT* create_propvariant(){
+    return new NWindows::NCOM::CPropVariant();
+}
+void destroy_propvariant(PROPVARIANT* pvar){
+    ((NWindows::NCOM::CPropVariant*) pvar)->Clear();
+    delete pvar;
+}
+
+IInStream* create_instream_from_file(FILE* file){ return (IInStream*)(new CFileStream(file)); }
+IOuStream* create_oustream_from_file(FILE* file){ return (IOutStream*)(new CFileStream(file)); }
+
+HRESULT archive_open(
+    IInArchive* archive,
+    IInStream* in_stream,
+    void* data,
+    wchar_t* password,
+    _get_password_callback get_password_callback, /*optional, takes precedence over password */
+    _set_total_callback set_total_callback, /* optional */
+    _set_completed_callback set_completed_callback /* optional */)
+{
+    if (archive == NULL){ return E_ABORT; }
+    CArchiveOpenCallback* open_callback = new CArchiveOpenCallback(
+        data,
+        password,
+        get_password_callback,
+        set_total_callback,
+        set_completed_callback);
+
+    const UInt64 maxCheckStartPosition = 1 << 22;
+    archive->Open(in_stream, &maxCheckStartPosition, open_callback);
+    return S_OK;
+}
+
+HRESULT archive_close(IInArchive* archive){
+    return archive->Close();
+}
+
+void archive_release(IInArchive* archive){
+    delete archive;
+}
+
+HRESULT archive_get_num_items(IInArchive* archive, uint32_t* num_items) {
+    if(archive == NULL){ LOG_DEBUG("archive is NULL"); return 0; }; //FIXME
+    if(num_items == NULL){ LOG_DEBUG("num_items is NULL"); return 0; }; // FIXME
+
+    HRESULT res = archive->GetNumberOfItems(num_items);
+    LOG_DEBUG("-> num_items=%d", *num_items);
+    return res;
+}
+
+HRESULT archive_get_item_property_pvar(
+    IInArchive* archive, uint32_t index, PROPID prop_id, PROPVARIANT* pvar)
+{
+    //*pvar = create_propvariant();
+    if(pvar == NULL){ LOG_DEBUG("pvar is NULL"); return E_ABORT; }; //FIXME
+    return archive->GetProperty(index, prop_id, pvar);
+}
---
+++ clib7zip.h
@@ -0,0 +1,105 @@
+#ifndef CLIB7ZIP_H
+#define CLIB7ZIP_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "7zip/PropID.h"
+//CDEF START
+
+//GUIDs
+extern const GUID IID_IInArchive;
+
+void set_logger_cb(void(const char*));
+
+enum {
+    NArchive_kName = 0,
+    NArchive_kClassID,
+    NArchive_kExtension,
+    NArchive_kAddExtension,
+    NArchive_kUpdate,
+    NArchive_kKeepName,
+    NArchive_kStartSignature,
+    NArchive_kFinishSignature,
+    NArchive_kAssociate
+};
+
+//PROPVARIANT
+PROPVARIANT* create_propvariant();
+void destroy_propvariant(PROPVARIANT*);
+
+//-
+
+typedef struct IInArchive IInArchive;
+typedef struct IInStream  IInStream;
+typedef struct IOutStream IOutStream;
+
+// Globals
+HRESULT _GetNumberOfFormats(uint32_t*);
+HRESULT _GetNumberOfMethods(uint32_t *);
+HRESULT _GetMethodProperty(uint32_t index, uint32_t propID, PROPVARIANT * value);
+HRESULT _GetHandlerProperty2(uint32_t, uint32_t propID, PROPVARIANT *);
+HRESULT _CreateObject(const GUID *, const GUID *, void **);
+
+//Streams
+typedef HRESULT (*_stream_read_callback)
+    (void* self, void *data, uint32_t size, uint32_t *processedSize);
+typedef HRESULT (*_stream_write_callback)
+    (void* self, void *data, uint32_t size, uint32_t *processedSize);
+typedef HRESULT (*_stream_seek_callback)
+    (void* self, uint64_t offset, int32_t seekOrigin, uint64_t *newPosition);
+typedef HRESULT (*_stream_get_size_callback)(void* self, uint64_t *size);
+
+IInStream* create_instream_from_file(FILE* file);
+IOutStream* create_outstream_from_file(FILE* file);
+/*
+IInStream* create_instream_from_callbacks(void* data,
+    _stream_read_callback read_cb,
+    _stream_seek_callback seek_cb,
+    _stream_get_size_callback get_size_cb);
+*/
+
+//IArchiveOpenCallback
+typedef HRESULT (*_aopen_set_total_callback)
+    (void* self, const uint64_t * files, const uint64_t * bytes);
+typedef HRESULT (*_aopen_set_completed_callback)
+    (void* self, const uint64_t * files, const uint64_t * bytes);
+typedef HRESULT (*_aopen_get_password_callback)(void* self, wchar_t** password);
+
+//IArchiveExtractCallback
+HRESULT(*_aextract_set_total_callback)
+    (void* self, uint64_t total);
+HRESULT(*_aextract_set_completed_callback)
+    (void* self, const uint64_t *completeValue);
+HRESULT(*_aextract_get_stream_callback)
+    (void* self, uint32_t index, IOutStream **outStream,  int32_t askExtractMode);
+HRESULT(*_aextract_prepare_operation_callback)
+    (void* self, int32_t askExtractMode);
+HRESULT(*_aextract_set_operation_result_callback)
+    (void* self, int32_t resultEOperationResult);
+
+//IInArchive
+IInArchive* create_archive(const wchar_t* type);
+void archive_release(IInArchive* archive);
+
+
+HRESULT archive_get_num_items(IInArchive* archive, uint32_t* num_items);
+HRESULT archive_get_item_property_pvar(
+    IInArchive* archive, uint32_t index, uint32_t prop, PROPVARIANT* pvar);
+HRESULT archive_get_item_property_str(
+    IInArchive* archive, uint32_t index, uint32_t prop, wchar_t buf[], size_t buf_size);
+HRESULT archive_get_item_property_uint64(
+    IInArchive* archive, uint32_t index, uint32_t prop_id, uint64_t* out);
+
+HRESULT archive_close(IInArchive*);
+//CDEF END
+
+#ifdef __cplusplus
+}
+#endif
+#endif
